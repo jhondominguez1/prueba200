f69bdf40039572e0483137da9c4702c8a1ff3dc3
jhondominguez1 <jjjdomi@gmail.com>
2016-05-14 15:26:28 -0500

    adiciono dos lineas a index2.php

---
+++ index2.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\prueba200
+
+14/05/2016  03:25 p. m.    <DIR>          .
+14/05/2016  03:25 p. m.    <DIR>          ..
+14/05/2016  03:25 p. m.                 0 index2.php
+               1 archivos              0 bytes
+               2 dirs  76.761.067.520 bytes libres
